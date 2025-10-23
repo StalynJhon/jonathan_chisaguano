06d5f654eb6b09248d597a2540305f6340d3dffc
StalynJhon <jsc.chisaguano@yavirac.edu.ec>
2025-10-23 09:53:48 -0500

    segundo

--- test.py
+++ test.py
@@ -11,15 +11,15 @@ class TestApp(unittest.TestCase):
     def test_respuesta_exitosa(self):
         # Verifica que la ruta principal responda con código 200
         respuesta = self.app.get('/')
-        self.assertEqual(respuesta.status_code, 200)
+        self.assertEqual(respuesta.status_code, 404)
 
     def test_contiene_primos_correctos(self):
         # Verifica que los números primos esperados estén en la respuesta
         respuesta = self.app.get('/')
         texto = respuesta.get_data(as_text=True)
-        self.assertIn('2', texto)
-        self.assertIn('19', texto)
-        self.assertNotIn('1', texto)  # 1 no es primo
+        self.assertIn('3', texto)
+        self.assertIn('15', texto)
+        self.assertNotIn('8', texto)  # 1 no es primo
 
 if __name__ == '__main__':
     unittest.main()
